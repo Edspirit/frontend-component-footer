89e7f06b6b93dda71faa42caa258db4446d1b3e9
milad-emami <milad.emami1025@gmail.com>
2022-12-24 13:08:26 +0330

    feat: new footer ui

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,72 +1,111 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { injectIntl, intlShape } from '@edx/frontend-platform/i18n';
-import { sendTrackEvent } from '@edx/frontend-platform/analytics';
-import { ensureConfig } from '@edx/frontend-platform/config';
-import { AppContext } from '@edx/frontend-platform/react';
+import React from "react";
+import PropTypes from "prop-types";
+import { injectIntl, intlShape } from "@edx/frontend-platform/i18n";
+import { sendTrackEvent } from "@edx/frontend-platform/analytics";
+import { ensureConfig } from "@edx/frontend-platform/config";
+import { AppContext } from "@edx/frontend-platform/react";
 
-import messages from './Footer.messages';
-import LanguageSelector from './LanguageSelector';
+import messages from "./Footer.messages";
+import LanguageSelector from "./LanguageSelector";
 
-ensureConfig([
-  'LOGO_TRADEMARK_URL',
-], 'Footer component');
+ensureConfig(["LMS_BASE_URL", "LOGO_TRADEMARK_URL"], "Footer component");
 
 const EVENT_NAMES = {
-  FOOTER_LINK: 'edx.bi.footer.link',
+  FOOTER_LINK: "edx.bi.footer.link",
 };
 
 class SiteFooter extends React.Component {
   constructor(props) {
     super(props);
     this.externalLinkClickHandler = this.externalLinkClickHandler.bind(this);
+    this.state = {
+      result: undefined,
+      footerLegal: undefined,
+      footerNav: undefined,
+    };
   }
 
-  getLocalePrefix(locale) {
-    const twoLetterPrefix = locale.substring(0, 2).toLowerCase();
-    if (twoLetterPrefix === 'en') {
-      return '';
-    }
-    return `/${twoLetterPrefix}`;
+  componentDidMount() {
+    this.fetchFooterData();
   }
 
   externalLinkClickHandler(event) {
-    const label = event.currentTarget.getAttribute('href');
+    const label = event.currentTarget.getAttribute("href");
     const eventName = EVENT_NAMES.FOOTER_LINK;
     const properties = {
-      category: 'outbound_link',
+      category: "outbound_link",
       label,
     };
     sendTrackEvent(eventName, properties);
   }
 
+  fetchFooterData() {
+    fetch(
+      `${this.context.config.LMS_BASE_URL}${this.context.config.AC_INSTANCE_CONFIG_API_URL}`
+    )
+      .then((response) => response.json())
+      .then((response) => {
+        this.setState({
+          result: JSON.parse(response),
+          footerLegal: JSON.parse(JSON.parse(response).footer_legal_links),
+          footerNav: JSON.parse(JSON.parse(response).footer_nav_links),
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
   render() {
-    const {
-      supportedLanguages,
-      onLanguageSelected,
-      logo,
-      intl,
-    } = this.props;
-    const showLanguageSelector = supportedLanguages.length > 0 && onLanguageSelected;
+    const { supportedLanguages, onLanguageSelected, logo, intl } = this.props;
+    const showLanguageSelector =
+      supportedLanguages.length > 0 && onLanguageSelected;
     const { config } = this.context;
 
     return (
-      <footer
-        role="contentinfo"
-        className="footer d-flex border-top py-3 px-4"
-      >
+      <footer role="contentinfo" className="footer d-flex border-top py-3 px-4">
         <div className="container-fluid d-flex">
-          <a
-            className="d-block"
-            href="https://open.edx.org"
-            aria-label={intl.formatMessage(messages['footer.logo.ariaLabel'])}
-          >
-            <img
-              style={{ maxHeight: 45 }}
-              src={logo || config.LOGO_TRADEMARK_URL}
-              alt={intl.formatMessage(messages['footer.logo.altText'])}
-            />
-          </a>
+          <div className="footer-wrapper">
+            <div className="footer-nav">
+              <div className="footer-nav-links">
+                <ul className="list-unstyled p-0 m-0 nav-list">
+                  {this.state.footerNav?.map((nav) => (
+                    <li key={nav?.title}>
+                      <a href={nav?.link}>{nav?.title}</a>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+              <div className="footer-legal-links">
+                <ul className="list-unstyled p-0 m-0 nav-list">
+                  {this.state.footerLegal?.map((nav) => (
+                    <li key={nav?.title}>
+                      <a href={nav?.link}>{nav?.title}</a>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+            <div className="logo-wrapper">
+              <a href="https://edspirit.com" aria-label="edspirit logo">
+                <img
+                  className="edspirit-logo"
+                  src={`${config.LMS_BASE_URL}${this.state.result?.edspirit_logo}`}
+                  alt="edX Logo"
+                />
+              </a>
+              <a href="https://open.edx.org">
+                <img
+                  className="openEdx-logo"
+                  src={
+                    `${config.LMS_BASE_URL}${this.state.result?.openedx_logo}` ||
+                    config.LOGO_TRADEMARK_URL
+                  }
+                  alt={intl.formatMessage(messages["footer.logo.altText"])}
+                />
+              </a>
+            </div>
+          </div>
           <div className="flex-grow-1" />
           {showLanguageSelector && (
             <LanguageSelector
@@ -86,10 +125,12 @@ SiteFooter.propTypes = {
   intl: intlShape.isRequired,
   logo: PropTypes.string,
   onLanguageSelected: PropTypes.func,
-  supportedLanguages: PropTypes.arrayOf(PropTypes.shape({
-    label: PropTypes.string.isRequired,
-    value: PropTypes.string.isRequired,
-  })),
+  supportedLanguages: PropTypes.arrayOf(
+    PropTypes.shape({
+      label: PropTypes.string.isRequired,
+      value: PropTypes.string.isRequired,
+    })
+  ),
 };
 
 SiteFooter.defaultProps = {
