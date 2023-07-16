03efce0b5c684c3e93a8692bf1d97c4ddb1ece3d
milad-emami <milad.emami1025@gmail.com>
2023-07-16 15:22:15 +0330

    fix: add new paragon

--- src/components/useGetActiveLangs.js
+++ src/components/useGetActiveLangs.js
@@ -1,25 +1,34 @@
 import { getConfig } from '@edx/frontend-platform';
 import { getAuthenticatedHttpClient } from '@edx/frontend-platform/auth';
 import { getLocale } from '@edx/frontend-platform/i18n';
-import { useState } from 'react';
-import { useQuery } from 'react-query';
+import { useEffect, useState } from 'react';
 
 const useGetActiveLangs = () => {
+  const [activeLangs, setActiveLangs] = useState(null);
+  const [loading, setLoading] = useState(true);
   const [locale, setLocale] = useState(getLocale());
-  const fetchActiveLangs = async () => {
-    const client = getAuthenticatedHttpClient();
-    const baseUrl = getConfig().LMS_BASE_URL;
-    const response = await client.get(`${baseUrl}/admin-console/api/active-langs/`);
-    const activeLangs = JSON.parse(response.data);
-    const newLocale = activeLangs && activeLangs[0] && activeLangs[0].code;
-    setLocale(newLocale);
-    return activeLangs;
-  };
-  const { data, isLoading } = useQuery('ActiveLangs', fetchActiveLangs);
 
+  useEffect(() => {
+    const fetchActiveLangs = async () => {
+      setLoading(true);
+      const client = getAuthenticatedHttpClient();
+      const baseUrl = getConfig().LMS_BASE_URL;
+      const response = await client.get(
+        `${baseUrl}/admin-console/api/active-langs/`,
+      );
+      const newActiveLangs = JSON.parse(response.data);
+      const newLocale = newActiveLangs && newActiveLangs[0] && newActiveLangs[0].code;
+      setActiveLangs(activeLangs);
+      setLocale(newLocale);
+      setLoading(false);
+    };
+    if (getConfig().LMS_BASE_URL) {
+      fetchActiveLangs();
+    }
+  }, [getConfig().LMS_BASE_URL]);
   return {
-    activeLangs: data,
-    loading: isLoading,
+    activeLangs,
+    loading,
     locale,
   };
 };
